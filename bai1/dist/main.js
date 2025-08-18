473354bd46aa77a6c64a28406b7d16f33f869194
Nguyen Van Tuan <nguyenvantuanvp1999@gmail.com>
2025-08-18 16:50:47 +0700

    add

---
+++ bai1/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let width;
+let height;
+width = 10.5;
+height = 20;
+let area = width * height;
+console.log(`Dien tich hinh chu nhat ${area}`);
+//# sourceMappingURL=main.js.map
