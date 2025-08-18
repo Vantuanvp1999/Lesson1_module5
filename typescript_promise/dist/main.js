44b828a992b3d8c16ed314b6aca8ffe23e7402d0
Vantuanvp1999 <nguyenvantuanvp1999@gmail.com>
2025-08-18 21:12:11 +0700

    add promise

---
+++ typescript_promise/dist/main.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let money = 10000;
+const buyACar = (car) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(() => {
+            if (money >= 10000) {
+                resolve("can buy " + car);
+            }
+            else {
+                reject("do not enough money");
+            }
+        }, 100);
+    }));
+};
+money = 1000001;
+const promise = buyACar("Vinfast").then(value => {
+    console.log(value);
+}, error => {
+    console.log(error);
+});
+//# sourceMappingURL=main.js.map
